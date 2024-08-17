73d64d81f79846c650014919f4a40e075d452028
Menno van Tol <menvantol@gmail.com>
2024-08-17 23:36:28 +0200

    general stuff

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,11 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- When deleting, don't copy
+keymap.set("n", "x", '"_x')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Tabs
+-- keymap.set("n", "<tab>", ":tabnext<cr>", opts )
+-- keymap.set("n", "<s-tab>", ":tabprev<cr>", opts )
