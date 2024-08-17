5c442eff60c781079388f7ff809b99cf8aca7ba5
Menno van Tol <menvantol@gmail.com>
2024-08-18 00:48:32 +0200

    add oil, disable transparency

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = {}
 
 config.color_scheme = 'Catppuccin Macchiato'
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.90
+config.window_background_opacity = 1
 config.font = wezterm.font 'Comic Code'
 config.font_size = 14.0
 
