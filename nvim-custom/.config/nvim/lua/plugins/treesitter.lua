d902bc68bf4d7f27e14875a567eaeb02ab56c77a
mvantol <mvantol@doctorzipp.it>
2024-08-28 21:47:17 +0200

    add treejs

--- nvim-custom/.config/nvim/lua/plugins/treesitter.lua
+++ nvim-custom/.config/nvim/lua/plugins/treesitter.lua
@@ -1,13 +1,24 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
-	dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+		dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+	},
+	{
+		"Wansmer/treesj",
+		keys = { "<space>m", "<space>j", "<space>s" },
+		dependencies = { "nvim-treesitter/nvim-treesitter" }, -- if you install parsers with `nvim-treesitter`
+		config = function()
+			require("treesj").setup({--[[ your config ]]
+			})
+		end,
+	},
 }
