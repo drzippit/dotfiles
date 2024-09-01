bd6c6e8d74a0282db77c6f4ce550b888b899bef0
Menno van Tol <menvantol@gmail.com>
2024-09-01 22:11:08 +0200

    tmux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,11 +1,11 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'Catppuccin Macchiato'
+config.color_scheme = "Catppuccin Macchiato"
 config.hide_tab_bar_if_only_one_tab = true
 config.window_background_opacity = 0.7
 config.macos_window_background_blur = 40
-config.font = wezterm.font 'Comic Code'
+config.font = wezterm.font("Comic Code")
 config.font_size = 14.0
 config.window_decorations = "RESIZE"
 
