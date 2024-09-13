d9ca9cb1593df78e54f67fc3f7987a041f10e240
Menno van Tol <menvantol@gmail.com>
2024-09-13 15:04:37 +0200

    move git stuff together

--- nvim-custom/.config/nvim-custom/lua/plugins/git.lua
+++ nvim-custom/.config/nvim-custom/lua/plugins/git.lua
@@ -1,6 +1,25 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	opts = function()
-		require("gitsigns").setup()
-	end,
+  {
+    "lewis6991/gitsigns.nvim",
+    opts = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    keys = {
+      { "<leader>gg", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit Current File" },
+      { "<leader>gG", "<cmd>LazyGit<cr>",            desc = "LazyGit cwd" },
+    },
+  },
 }
