a763c34501a8b1b907a2ef7b90e1d8caa747b8fc
Menno van Tol <menvantol@gmail.com>
2024-10-02 21:36:35 +0200

    add block select

--- nvim-custom/.config/nvim-custom/lua/plugins/treesitter.lua
+++ nvim-custom/.config/nvim-custom/lua/plugins/treesitter.lua
@@ -39,6 +39,8 @@ return {
 							["if"] = "@function.inner",
 							["ac"] = "@class.outer",
 							["ic"] = "@class.inner",
+							["ib"] = "@block.inner",
+							["ab"] = "@block.outer",
 						},
 					},
 				},
