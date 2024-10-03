8a750e727ed72387efdef8e84110ce6cfd9a70e9
Menno van Tol <menvantol@gmail.com>
2024-10-03 10:52:04 +0200

    add folding with UFO

--- nvim-custom/.config/nvim-custom/lua/config/lazy.lua
+++ nvim-custom/.config/nvim-custom/lua/config/lazy.lua
@@ -31,6 +31,10 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.scrolloff = 10
 vim.opt.conceallevel = 0
+vim.o.foldcolumn = "0" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
