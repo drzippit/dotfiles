dc94f1e9ceb8c50403af4b53ba9f686f45ea653a
Menno van Tol <menvantol@gmail.com>
2025-02-11 23:26:27 +0100

    lazygit config inital

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    -- change a keymap
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<leader> ", "<cmd>Telescope buffers<cr>", desc = "Telescope Buffers" },
+  },
+}
