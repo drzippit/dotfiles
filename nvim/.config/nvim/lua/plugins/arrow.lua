bf75273fd151c6386e4b2523d7631246c714bdaf
Menno van Tol <menvantol@gmail.com>
2025-02-14 14:46:43 +0100

    snacks explorer and arrow lazy

--- nvim/.config/nvim/lua/plugins/arrow.lua
+++ nvim/.config/nvim/lua/plugins/arrow.lua
@@ -1,5 +1,9 @@
 return {
   "otavioschwanck/arrow.nvim",
+  lazy = true,
+  keys = {
+    { "\\", "<cmd>Arrow open", desc = "Open Arrow" },
+  },
   opts = {
     show_icons = true,
     leader_key = "\\", -- Recommended to be a single key
