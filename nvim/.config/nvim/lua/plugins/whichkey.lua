a83b10751cad25566a671119a5aa9f33d3fb1aae
Menno van Tol <menvantol@gmail.com>
2025-03-09 20:21:57 +0100

    add oil and icons to groups

---
+++ nvim/.config/nvim/lua/plugins/whichkey.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      { "<leader>o", group = "Obsidian", icon = "" },
+      { "<leader>t", group = "Terraform", icon = "󱁢" },
+    },
+  },
+}
