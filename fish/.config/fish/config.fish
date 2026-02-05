ca7d8606bfd19dc8dcd900f1c9c63ee36a396c50
Zippit <drzippit@gmail.com>
2026-02-05 09:22:36 +0100

    add pomodoro timer to fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -97,6 +97,35 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 starship init fish | source
 
 fish_add_path -m ~/.local/bin
+fish_add_path -m /opt/homebrew/bin
+
+# Pomodoro Timer: https://gist.github.com/bashbunni/e311f07e100d51a883ab0414b46755fa
+function pom
+    set split $POMO_SPLIT
+    if ! test -n "$split"
+        set split $(gum choose "25/5" "50/10" "all done" --header "Choose a pomodoro split.")
+    end
+
+    switch $split
+        case 25/5
+            set work 25m
+            set break 5m
+        case 50/10
+            set work 50m
+            set break 10m
+        case 'all done'
+            return
+    end
+
+    timer $work && terminal-notifier -message Pomodoro \
+        -title 'Work Timer is up! Take a Break 😊' \
+        -sound Crystal
+
+    gum confirm "Ready for a break?" && timer $break && terminal-notifier -message Pomodoro \
+            -title 'Break is over! Get back to work 😬' \
+        -sound Crystal \
+        || pom
+end
 
 # overwrite greeting
 # potentially disabling fastfetch
